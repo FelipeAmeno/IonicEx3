60a08689e884e0a831f556c4d2d848ef4324de70
Felipe <felipeameno2@gmail.com>
2020-06-07 23:25:26 -0300

    Acabei tudo ate a 5

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { PessoaService } from '../services/pessoa.service'
+import { Pessoas } from '../model/pessoas.model'
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,11 +11,32 @@ import { PessoaService } from '../services/pessoa.service'
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  pessoas: Pessoas[] = [];
 
-  constructor(private pessoaService: PessoaService) { }
+  constructor(private pessoaService: PessoaService, private geolocation: Geolocation) { }
 
   async ngOnInit() {
-    let teste = await this.pessoaService.getPessoa();
-    console.log(teste);
+    await this.geolocation.getCurrentPosition().then((resp) => {
+      console.log("Localização");
+
+      console.log("Latitude: " + resp.coords.latitude + " Longitude: " + resp.coords.longitude)
+      this.ChamarpessoasProximas(resp.coords.latitude, resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Erro ao encontrar/transmitir a localização ', error);
+    });
+  }
+
+  async ChamarpessoasProximas(lat, long) {
+    await this.pessoaService.encontrarPessoasPessoasProxima(lat, long).then((x) => {
+      console.log(x);
+      x.forEach(x => {
+        console.log(x);
+        console.log(x.data);
+        this.pessoas.push(x.data);
+      })
+      console.log(this.pessoas);
+    }).catch((err) => {
+      console.log(err);
+    })
   }
 }
